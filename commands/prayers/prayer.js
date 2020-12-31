b0b9b79af0f7b07be0125aa50dd89af5386b6dc8
mxr <eetu.kivelae@gmail.com>
2020-12-31 16:15:22 +0200

    Added command for playing prayers

---
+++ commands/prayers/prayer.js
@@ -0,0 +1,28 @@
+const PrayerCommandBase = require('./PrayerCommandBase');
+
+class PrayerCommand extends PrayerCommandBase {
+  constructor(client) {
+    super(client, {
+      name: 'prayer',
+      group: 'prayers',
+      memberName: 'prayer',
+      description: 'Play a prayer | العب صلاة',
+      hidden: false
+    });
+    this.name = 'prayer';
+
+    this.volume = 0.32;
+    this.logger = client.logger;
+  }
+
+  
+
+  run(msg) {
+    if(msg.member.voice.channel)
+      this.playPrayer(msg);
+    else
+      msg.reply('الانضمام إلى قناة');
+  }
+}
+
+module.exports = PrayerCommand;
